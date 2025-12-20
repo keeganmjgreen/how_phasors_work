5e11ec0951137f023afc77c3710e4c3f4e465a0a
Keegan Green <keeganmjgreen@gmail.com>
2025-12-20 13:33:28 -0500

    simultaneously support builds using Jupyter Book V1 (for PDF) in addition to Jupyter Book V2 (for HTML)

--- build_book.sh
+++ build_book.sh
@@ -1,7 +1,7 @@
-# Build static HTML and PDF (Jupyter Book V1):
+# Build static PDF (Jupyter Book V1):
 source .venv-jb1/bin/activate
-jupyter-book build .
-sed -i 's#!\[](img/cover.png)#<!-- ![](img/cover.png) -->#' 0_prelude.md
+git revert --no-edit 4bbbb54550655d4a5d7a9193ee4c04d1719cd2b8
 jupyter-book build . --builder=pdflatex
-sed -i 's#<!-- !\[](img/cover.png) -->#![](img/cover.png)#' 0_prelude.md
 pdfunite cover/cover.pdf cover/blank.pdf _build/latex/book.pdf how_phasors_work.pdf
+git reset
+git reset --keep HEAD~1
